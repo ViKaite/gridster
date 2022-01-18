6488bc218251a7884f6dbf3998cdb17f9ad8ab47
ViKaite <viktorija.zelionkaite@gmail.com>
2022-01-18 11:21:11 +0200

    comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+    <div className="navBar">
+
+        <label className="d-flex column">
+            Row:
+            <input type="text" name="row" />
+        </label>
+        <p className="margin">x</p>
+        <label className="d-flex column">
+            Column:
+            <input type="text" name="column" />
+        </label>
+        <input className="inputBtn " type="submit" value="Generate" />
+
+   </div>
+   </div>
+
   );
 }
 
